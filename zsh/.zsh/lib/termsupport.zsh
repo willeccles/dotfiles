7c310fe6583e3047e00de02f01b658676ddfe577
Will Eccles <will@eccles.dev>
2021-12-03 23:06:58 -0500

    fix up macos stuff

--- zsh/.zsh/lib/termsupport.zsh
+++ zsh/.zsh/lib/termsupport.zsh
@@ -83,23 +83,23 @@ preexec_functions+=(omz_termsupport_preexec)
 # Based on this answer: https://superuser.com/a/315029
 # With extra fixes to handle multibyte chars and non-UTF-8 locales
 
-if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]]; then
-  # Emits the control sequence to notify Terminal.app of the cwd
-  # Identifies the directory using a file: URI scheme, including
-  # the host name to disambiguate local vs. remote paths.
-  function update_terminalapp_cwd() {
-    emulate -L zsh
-
-    # Percent-encode the pathname.
-    local URL_PATH="$(omz_urlencode -P $PWD)"
-    [[ $? != 0 ]] && return 1
-
-    # Undocumented Terminal.app-specific control sequence
-    printf '\e]7;%s\a' "file://$HOST$URL_PATH"
-  }
-
-  # Use a precmd hook instead of a chpwd hook to avoid contaminating output
-  precmd_functions+=(update_terminalapp_cwd)
-  # Run once to get initial cwd set
-  update_terminalapp_cwd
-fi
+#if [[ "$TERM_PROGRAM" == "Apple_Terminal" ]] && [[ -z "$INSIDE_EMACS" ]]; then
+#  # Emits the control sequence to notify Terminal.app of the cwd
+#  # Identifies the directory using a file: URI scheme, including
+#  # the host name to disambiguate local vs. remote paths.
+#  function update_terminalapp_cwd() {
+#    emulate -L zsh
+#
+#    # Percent-encode the pathname.
+#    local URL_PATH="$(omz_urlencode -P $PWD)"
+#    [[ $? != 0 ]] && return 1
+#
+#    # Undocumented Terminal.app-specific control sequence
+#    printf '\e]7;%s\a' "file://$HOST$URL_PATH"
+#  }
+#
+#  # Use a precmd hook instead of a chpwd hook to avoid contaminating output
+#  precmd_functions+=(update_terminalapp_cwd)
+#  # Run once to get initial cwd set
+#  update_terminalapp_cwd
+#fi
