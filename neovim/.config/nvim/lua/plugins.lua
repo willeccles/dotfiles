e6d1391c7cb220ed8a3277daf615fa1e9cd38021
Will Eccles <will@eccles.dev>
2023-08-07 15:13:56 -0400

    nvim: remap mini.trailspace again

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -354,7 +354,7 @@ local plugins = {
     opts = {},
     keys = {
       {
-        '<Leader>T',
+        '<Leader>s',
         function()
           require'mini.trailspace'.trim()
           require'mini.trailspace'.trim_last_lines()
