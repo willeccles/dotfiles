d270ce3b72c52c315d75dcd3b5dd6167118956ad
Will Eccles <will@eccles.dev>
2023-08-10 13:11:59 -0400

    Revert "nvim: remove zig plugin"
    This reverts commit c3f8e3f990f25e9b9b2d2f4f3dbd137f1dfc7df9.

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -294,6 +294,14 @@ local plugins = {
   },
 
   {
+    "ziglang/zig.vim",
+    ft = 'zig',
+    config = function()
+      vim.g.zig_fmt_autosave = 0
+    end,
+  },
+
+  {
     'kyazdani42/nvim-tree.lua',
     tag = 'nightly',
     keys = {
